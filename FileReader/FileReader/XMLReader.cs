b2dbe7d987841e09a449231a80a0d5718090cc15
Wissem Kallel <wissem_27@hotmail.com>
2018-06-15 23:01:20 +0100

    Added support for reading XML files. Closes #2.

---
+++ FileReader/FileReader/XMLReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FileReader
+{
+    /// <summary>
+    /// File Reader to read XML files
+    /// </summary>
+    public class XMLReader : IFileReader
+    {
+        public bool TryReadFile(string filePath, out string fileContent)
+        {
+            fileContent = "";
+
+            try
+            {
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    fileContent = "Error reading the file: File path null or empty";
+                    return false;
+                }
+
+                ///Load the xml file
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.PreserveWhitespace = true;
+                xmlDoc.Load(filePath);
+                fileContent = xmlDoc.OuterXml;
+                return true;
+            }
+            catch (Exception e)
+            {
+                fileContent = "Error reading the file: " + e.Message;
+                return false;
+            }
+        }
+    }
+}
---
+++ FileReader/FileReaderTests/XMLReaderTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using System.Xml;
+
+namespace FileReader.Tests
+{
+    [TestClass()]
+    public class XMLReaderTests
+    {
+        /// <summary>
+        /// This test expects reading XML to return false when the file path is null or empty
+        /// </summary>
+        [TestMethod()]
+        public void TestUnsuccessfulReadXMLEmptyPath()
+        {
+            using (ShimsContext.Create())
+            {
+                ///Arrange
+                System.Xml.Fakes.ShimXmlDocument.AllInstances.LoadString = (@this, s) => { };
+                System.Xml.Fakes.ShimXmlDocument.AllInstances.PreserveWhitespaceSetBoolean = (@this, s) => { };
+
+                ///Act
+                string content;
+                var result = sut.TryReadFile("", out content);
+
+                ///Assert
+                Assert.AreEqual(false, result);
+            }
+        }
+
+        /// <summary>
+        /// This test expects reading XML to return false when the file loading fails by throwing an exception
+        /// </summary>
+        [TestMethod()]
+        public void TestUnsuccessfulReadXMLException()
+        {
+            using (ShimsContext.Create())
+            {
+                ///Arrange
+                System.Xml.Fakes.ShimXmlDocument.AllInstances.LoadString = (@this, s) => { throw new Exception(); };
+                System.Xml.Fakes.ShimXmlDocument.AllInstances.PreserveWhitespaceSetBoolean = (@this, s) => { };
+
+                ///Act
+                string content;
+                var result = sut.TryReadFile("path", out content);
+
+                ///Assert
+                Assert.AreEqual(false, result);
+            }
+        }
+
+        /// <summary>
+        /// This test expects reading XML to return true when the file reading succeeds
+        /// </summary>
+        [TestMethod()]
+        public void TestSuccessfulReadXML()
+        {
+            using (ShimsContext.Create())
+            {
+                ///Arrange
+                System.Xml.Fakes.ShimXmlDocument.AllInstances.LoadString = (@this, s) => { };
+                System.Xml.Fakes.ShimXmlDocument.AllInstances.PreserveWhitespaceSetBoolean = (@this, s) => { };
+
+
+                ///Act
+                string content;
+                var result = sut.TryReadFile("path", out content);
+
+                ///Assert
+                Assert.AreEqual(true, result);
+            }
+        }
+
+        private XMLReader sut = new XMLReader();
+    }
+}
